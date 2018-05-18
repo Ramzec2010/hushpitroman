92a7410ced983c8093920c5da4ddb113fd47fa29
Roman <38570605+Ramzec2010@users.noreply.github.com>
2018-05-18 15:21:14 +0300

    Add files via upload

--- com/romanhushpit/fantasticfour/SecretaryJenifer.java
+++ com/romanhushpit/fantasticfour/SecretaryJenifer.java
@@ -2,20 +2,31 @@ package com.romanhushpit.fantasticfour;
 
 public class SecretaryJenifer {
     public static void main(String[] args) {
-        int sec = timePrinter(1,1,4);
+        int sec = timePrinter(1,2,5);
+        System.out.println(sec);
     }
 
     static int timePrinter (int x, int y, int N) {
-        int minT = 0;
-        int numOfCopies = 0;
-        if (x > y)
-            minT = y;
-        else
-            minT = x;
-        int timeForCopys = minT;
-        numOfCopies++;
-        for (int i = 1; )
-
-
+        int max, min;
+        int comparison = 0;
+        int numOfCurCopies = 0;
+        int summTime = 0;
+        if (x > y) {max = x; min = y;}
+        else {max = y; min = x;}
+        int timeForOneCopy = min;
+        N = N - 1;
+        for (int i = 0; i < N; i++) {
+            summTime = summTime + min;
+            comparison = comparison + min;
+            numOfCurCopies++;
+            if (numOfCurCopies >= N) break;
+            if (comparison >= max){
+                comparison = 0;
+                numOfCurCopies++;
+            }
+            if (numOfCurCopies >= N) break;
+        }
+        summTime = summTime + timeForOneCopy;
+        return summTime;
     }
 }
