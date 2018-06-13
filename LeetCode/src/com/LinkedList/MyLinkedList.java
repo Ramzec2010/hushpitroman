5673a2ff916e429e5f2b5958a4a23a53bd11b61d
Roman <38570605+Ramzec2010@users.noreply.github.com>
2018-06-13 17:59:49 +0300

    Add files via upload

---
+++ LeetCode/src/com/LinkedList/MyLinkedList.java
@@ -0,0 +1,116 @@
+package com.LinkedList;
+
+class MyLinkedList {
+    private Node head;
+    //private Node tail;
+    private int length;
+
+
+    public MyLinkedList() {
+        head = null;
+        length = 0;
+        }
+
+
+    public int get(int index) {
+        Node curr = this.head;
+        if(head == null || index >= this.length) {
+            return -1;
+        }
+        else {
+            for (int i = 0; i < index; i++){
+                curr = curr.next;
+            }
+        }
+        return curr.val;
+    }
+
+    public void addAtHead(int val) {
+        Node curr = new Node(val);
+        curr.next = this.head;
+        this.head = curr;
+        this.length++;
+    }
+
+
+    public void addAtTail(int val) {
+        Node last = this.head;
+        Node curr = new Node(val);
+        while (last.next != null ) {
+            last = last.next;
+        }
+        last.next = curr;
+        this.length++;
+    }
+
+
+    public void addAtIndex(int index, int val) {
+        if (index > this.length) return;
+        if (index == this.length) {
+            addAtTail(val);
+            return;
+        }
+        if (index == 0) {
+            addAtHead(val);
+            return;
+        }
+        int i = 0;
+        Node curr = this.head;
+        while(i < index - 1) {
+            i++;
+            curr = curr.next;
+        }
+        Node prev = curr;
+        Node next = curr.next;
+        Node newNode = new Node(val);
+        newNode.next = next;
+        prev.next = newNode;
+        this.length++;
+    }
+
+    public void deleteAtIndex(int index) {
+        if (index > length || index < 0) return;
+        if (index == length){
+            Node last = this.head;
+            for (int i = 0; i < length - 1; i++){
+                last = last.next;
+            }
+            last.next = null;
+            length--;
+        }
+        Node curr = this.head;
+        for (int i = 1; i < index -1; i++) {
+            curr = curr.next;
+        }
+        Node curr_1 = curr.next;
+        curr_1 = curr_1.next;
+        curr.next = curr_1;
+        this.length--;
+    }
+    public int getSize() {
+        return this.length;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    class Node {
+        int val;
+        Node next;
+        Node(int x) { val = x; }
+
+    }
+
+    public static void main(String[] args) {
+        MyLinkedList linkedList = new MyLinkedList();
+        int j = linkedList.get(0);
+        linkedList.addAtHead(1);
+        linkedList.addAtTail(3);
+        linkedList.addAtIndex(1,2);
+        int i  =  linkedList.get(1);
+        linkedList.deleteAtIndex(1);
+        i = linkedList.get(1);
+    }
+}
